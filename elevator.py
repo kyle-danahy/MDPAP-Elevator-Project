ff486bc3e7e9873542b410a709baeb7d1c9a2d6a
Kyle Danahy <kdanahy33@gmail.com>
2025-03-06 18:23:27 -0700

    Added handling for non-numeric values in the floors list. Created tests to validate that behavior

--- elevator.py
+++ elevator.py
@@ -1,17 +1,24 @@
-# ASSUMPTIONS: all floors are non-negative
-#               all floors are integers
+# ASSUMPTIONS: all floors are integers, should be greater than 0 but I added a check just in case
 from argparse import ArgumentParser
 
 # constant for time to travel a single floor
 SINGLE_FLOOR_TRAVEL_TIME = 10
 
 def calculate_travel_time(floors_to_visit):
+    # assuming the first floor of a building should be 1, should not allow a floor number of 0
+    if 0 in floors_to_visit:
+        return "0 is not a valid floor number, please choose positive values only"
+
     total_travel_time = 0
-    # start range at 1 since we don't need to calculate travel time for starting floor
+    # start at index 1 since we don't need to calculate travel time for starting floor
     for index in range(1, len(floors_to_visit)):
-        total_travel_time = total_travel_time + (abs(floors_to_visit[index] - floors_to_visit[index - 1]) 
+        # check that each value is a number
+        if str(floors_to_visit[index]).isnumeric():
+            total_travel_time = total_travel_time + (abs(floors_to_visit[index] - floors_to_visit[index - 1]) 
                                                         * SINGLE_FLOOR_TRAVEL_TIME)
-        index = index + 1
+            index = index + 1
+        else:
+            return f"'{floors_to_visit[index]}' is not a valid floor number. Please make sure it is a positive integer."
     return {'total_travel_time': total_travel_time, 'floors_visited': floors_to_visit}
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -23,6 +23,21 @@ class TestCalculateTravelTime(unittest.TestCase):
         self.assertEqual(elevator.calculate_travel_time([1, 57, 109, 163, 25, 133]),
                          {'total_travel_time': 4080, 'floors_visited': [1, 57, 109, 163, 25, 133]})
 
+    def test_zero_first(self):
+        self.assertEqual(elevator.calculate_travel_time([0, 10]),
+                         "0 is not a valid floor number, please choose positive values only")
         
+    def test_zero_in_list(self):
+        self.assertEqual(elevator.calculate_travel_time([3, 17, 5, 0, 12]),
+                         "0 is not a valid floor number, please choose positive values only")
+        
+    def test_non_numeric(self):
+        self.assertEqual(elevator.calculate_travel_time([1, 'a', 3, 10]),
+                         "'a' is not a valid floor number. Please make sure it is a positive integer.")
+        
+    def test_negative_floor_number(self):
+        self.assertEqual(elevator.calculate_travel_time([10, 7, 33, -4, 15]),
+                         "'-4' is not a valid floor number. Please make sure it is a positive integer.")
+
 if __name__ == '__main__':
     unittest.main()
